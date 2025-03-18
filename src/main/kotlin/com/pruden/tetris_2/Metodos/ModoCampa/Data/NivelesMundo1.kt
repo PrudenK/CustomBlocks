69a1691290c25e04aa4b66d644d45d78f671e7b2
Pruden <prudencosta@gmail.com>
2025-03-18 19:59:15 +0100

    Niveles mundo 5

--- src/main/kotlin/com/pruden/tetris_2/Metodos/ModoCampa/Data/NivelesMundo1.kt
+++ src/main/kotlin/com/pruden/tetris_2/Metodos/ModoCampa/Data/NivelesMundo1.kt
@@ -18,7 +18,7 @@ val nivel1_7 =  Nivel("Mundo 1, Nivel 7",intArrayOf(0, 1, 2, 3, 4, 5, 6), 1200, 
     -1, true, 2, true, 1, true, 8000, "/0:04:00", 16, 4)
 val nivel1_8 =  Nivel("Mundo 1, Nivel 8",intArrayOf(0, 1, 2, 3, 4, 5, 6), 1300, 1, 100, false,
     -1, true, 2, true, 0, true, 5000, "/0:03:30", 10, 10)
-val nivel1_9 =  Nivel("Mundo 1, Nivel 8",intArrayOf(0, 1, 17, 22, 27), 1500, 4, 100, false,
+val nivel1_9 =  Nivel("Mundo 1, Nivel 9",intArrayOf(0, 1, 17, 22, 27), 1500, 4, 100, false,
     -1, true, 2, true, 1, true, 12000, "/0:04:30", 24, 6)
 
 val listaNivelesMundo1 = listOf(nivel1_1, nivel1_2, nivel1_3, nivel1_4, nivel1_5, nivel1_6, nivel1_7, nivel1_8, nivel1_9)
--- src/main/kotlin/com/pruden/tetris_2/Metodos/ModoCampa/Data/NivelesMundo5.kt
+++ src/main/kotlin/com/pruden/tetris_2/Metodos/ModoCampa/Data/NivelesMundo5.kt
@@ -4,6 +4,21 @@ import com.pruden.tetris_2.Metodos.ModoCampa.Nivel
 
 val nivel5_1 = Nivel("Mundo 5, Nivel 1",intArrayOf(2,4,6,11,15,27,28), 1200, 2, 100, false,
     -1, true, 1, true, 1, false, 4000, "/0:04:00", 20, 10)
+val nivel5_2 = Nivel("Mundo 5, Nivel 2",intArrayOf(2,4,6,11,15,27,28), 500, 5, 100, false,
+    -1, true, 1, true, 1, true, 1000, "/0:00:45", 5, 1)
+val nivel5_3 = Nivel("Mundo 5, Nivel 3",intArrayOf(2,4,6,11,15,27,28), 1000, 3, 300, true,
+    4, true, 1, true, 1, false, 2500, "/0:02:00", 9, 3)
+val nivel5_4 = Nivel("Mundo 5, Nivel 4",intArrayOf(2,4,6,11,15,27,28,34,35), 1200, 2, 150, false,
+    -1, true, 0, true, 1, false, 2500, "/0:03:00", 14, 7)
+val nivel5_5 = Nivel("Mundo 5, Nivel 5",intArrayOf(2,4,6,27,28,34,35,36,37), 1100, 2, 150, false,
+    -1, true, 2, true, 1, true, 3500, "/0:03:00", 12, 6)
+val nivel5_6 = Nivel("Mundo 5, Nivel 6",intArrayOf(2,4,6,27,34,35,36), 1500, 1, 100, false,
+    -1, true, 0, false, 2, false, 2500, "/0:01:30", 12, 12)
+val nivel5_7 = Nivel("Mundo 5, Nivel 7",intArrayOf(2,4,6,27,34,35,36), 250, 10, 100, false,
+    -1, true, 0, true, 2, true, 1200, "/0:01:00", 10, 1)
+val nivel5_8= Nivel("Mundo 5, Nivel 8",intArrayOf(2,4,6,27,34,35,36), 1500, 2, 100, false,
+    -1, true, 0, true, 3, false, 1800, "/0:01:30", 12, 6)
+val nivel5_9 = Nivel("Mundo 5, Nivel 9",intArrayOf(2,4,6,27,28,34,35,36,37), 400, 1, 50, false,
+    -1, true, 2, true, 1, true, 2700, "/0:01:45", 5, 5)
 
-
-val listaNivelesMundo5 = listOf(nivel5_1)
+val listaNivelesMundo5 = listOf(nivel5_1, nivel5_2, nivel5_3, nivel5_4, nivel5_5, nivel5_6, nivel5_7, nivel5_8, nivel5_9)
