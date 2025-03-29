311b4dc2bcca9f8c31641400ec4a9c0d19fa73ae
Pruden <prudencosta@gmail.com>
2025-03-29 11:03:31 +0100

    fix a la hora de recoger el modo de juego de la API

--- src/main/kotlin/com/pruden/tetris_2/API/ObjsAux/ModoDeJuegoAPI.kt
+++ src/main/kotlin/com/pruden/tetris_2/API/ObjsAux/ModoDeJuegoAPI.kt
@@ -2,23 +2,23 @@ package com.pruden.tetris_2.API.ObjsAux
 
 data class ModoDeJuegoAPI(
     val idJugador: Int,
-    val idNumModo: Int,
+    val idnummodo: Int,
     val nombre: String,
     val imagen: String,
-    val arrayPiezas: String,
+    val arraypiezas: String,
     val tablero: Int,
-    val tipoTableroPrincipal: Int,
-    val tipoPieza: Int,
-    val tipoTableroSecun: Int,
-    val tiempoCaidaInicial: Int,
-    val lineasParaSaltoNivel: Int,
-    val saltoDeTiempoPorNivel: Int,
-    val limiteRotaciones: Int,
+    val tipotableroprincipal: Int,
+    val tipopieza: Int,
+    val tipotablerosecun: Int,
+    val tiempocaidainicial: Int,
+    val lineasparasaltonivel: Int,
+    val saltodetiempopornivel: Int,
+    val limiterotaciones: Int,
     val hold: Int,
     val piezas: Int,
     val dashes: Int
 ){
     fun siguientesPiezas() = piezas == 1
     fun dashDisponible() = dashes == 1
-    fun hayLimiteRotaciones() = limiteRotaciones != -1
+    fun hayLimiteRotaciones() = limiterotaciones != -1
 }
