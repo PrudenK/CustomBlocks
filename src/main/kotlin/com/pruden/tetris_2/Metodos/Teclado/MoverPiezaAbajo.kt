d96991c44abec326a2561f87630757a9e8e1afec
Pruden <prudencosta@gmail.com>
2025-04-03 10:30:47 +0200

    Puntuaciones en visi y host

--- src/main/kotlin/com/pruden/tetris_2/Metodos/Teclado/MoverPiezaAbajo.kt
+++ src/main/kotlin/com/pruden/tetris_2/Metodos/Teclado/MoverPiezaAbajo.kt
@@ -3,8 +3,10 @@ package com.pruden.tetris_2.Metodos.Teclado
 import com.pruden.tetris_2.Metodos.SubirDatos.contadorPiezas
 import com.pruden.tetris_2.Metodos.SubirDatos.sumarTipoPieza
 import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.cPrin
+import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.eresHostPVP
 import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.jugarOnline
 import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.objPuntuacionBajar
+import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.partidaPVPenCurso
 import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.piezaActual
 import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.tipoTableroPrin
 import com.pruden.tetris_2.Metodos.Media.Audio.efectoSonido
@@ -14,7 +16,15 @@ import javafx.scene.input.KeyEvent
 
 fun bajar(){
     if (moverPiezaAbajo()) {
-        cPrin.labelPuntuacion.text = (cPrin.labelPuntuacion.text.split("/")[0].toInt() + 2).toString() + objPuntuacionBajar
+
+        //TODO posible refactor en un futuro
+        val label = when {
+            partidaPVPenCurso && eresHostPVP -> cPrin.puntuacionHostLabel
+            partidaPVPenCurso && !eresHostPVP -> cPrin.puntuacionVisiLabel
+            else -> cPrin.labelPuntuacion
+        }
+
+        label.text = (label.text.split("/")[0].toInt() + 2).toString() + objPuntuacionBajar
         efectoSonido("/Musica/Efectos/sonidoMoverPieza.mp3")
     }
 }
@@ -22,7 +32,15 @@ fun bajar(){
 fun moverEspacio(event : KeyEvent){
     event.consume()
     while (moverPiezaAbajo()) {
-       cPrin.labelPuntuacion.text = (cPrin.labelPuntuacion.text.split("/")[0].toInt() + 4).toString() + objPuntuacionBajar
+
+        val label = when {
+            partidaPVPenCurso && eresHostPVP -> cPrin.puntuacionHostLabel
+            partidaPVPenCurso && !eresHostPVP -> cPrin.puntuacionVisiLabel
+            else -> cPrin.labelPuntuacion
+        }
+
+
+        label.text = (label.text.split("/")[0].toInt() + 4).toString() + objPuntuacionBajar
     }
     efectoSonido("/Musica/Efectos/caidaPieza.mp3")
 }
