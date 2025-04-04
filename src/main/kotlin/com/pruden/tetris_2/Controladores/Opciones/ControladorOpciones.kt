53e3bd9f7ff6fa7de04d059546c95159601cfdd6
Pruden <prudencosta@gmail.com>
2025-04-04 14:06:31 +0200

    opciones en modo PVP

--- src/main/kotlin/com/pruden/tetris_2/Controladores/Opciones/ControladorOpciones.kt
+++ src/main/kotlin/com/pruden/tetris_2/Controladores/Opciones/ControladorOpciones.kt
@@ -7,6 +7,7 @@ import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.cPrin
 import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.cronometro
 import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.jugarOnline
 import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.partidaEnCurso
+import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.partidaPVPenCurso
 import com.pruden.tetris_2.Controladores.ControladorPrincipal.Companion.timelinePartida
 import com.pruden.tetris_2.Controladores.Partidas.ControladorPartidasGuardadas
 import com.pruden.tetris_2.Controladores.Suscripciones.ControladorSuscripciones.Companion.indiceSuscripciones
@@ -15,21 +16,40 @@ import com.pruden.tetris_2.Metodos.Stages.crearStage
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
 import javafx.scene.control.Button
+import javafx.scene.control.Label
 import javafx.stage.Stage
 import java.net.URL
 import java.util.*
 
 class ControladorOpciones : ControladorGEN(), Initializable{
+    @FXML lateinit var guardados: Button
+    @FXML lateinit var perfil: Button
+    @FXML lateinit var rankings: Button
+    @FXML lateinit var suscripciones: Button
+    @FXML lateinit var logros: Button
+    @FXML lateinit var guardar: Button
+    @FXML lateinit var reiniciar: Button
+
     companion object{
         lateinit var stageOpciones: Stage
     }
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
-        timelinePartida.stop()
-        cronometro.parar()
+        if(partidaPVPenCurso){
+            guardados.isDisable = true
+            perfil.isDisable = true
+            rankings.isDisable = true
+            suscripciones.isDisable = true
+            logros.isDisable = true
+            guardar.isDisable = true
+            reiniciar.isDisable = true
+        }else{
+            timelinePartida.stop()
+            cronometro.parar()
+        }
     }
     @FXML fun volver() {
-        if(partidaEnCurso){
+        if(partidaEnCurso && !partidaPVPenCurso){
             reanudarPartida()
         }
         stageOpciones.close()
@@ -43,7 +63,9 @@ class ControladorOpciones : ControladorGEN(), Initializable{
         stageOpciones.close()
     }
     @FXML fun reanudar() {
-        reanudarPartida()
+        if(!partidaPVPenCurso){
+            reanudarPartida()
+        }
         stageOpciones.close()
     }
 
